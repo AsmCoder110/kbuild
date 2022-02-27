bdc194c94cf6b7805f784ca293536cebe7961cdc
Yogendra Singh Kashyap <yskthecpplover@gmail.com>
2022-02-27 15:51:44 +0530

    + variables

--- build.sh
+++ build.sh
@@ -7,7 +7,17 @@ if [ -d build ]; then
 	rm -rfi build/* && \
 		cd build && \
 		wget "https://kernel.org/"
-			kURL=$(wget $(grep -E "cdn" -m 1 index.html | cut -d\" -f2)) && \
+
+		kURL=$(wget $(grep -E "cdn" -m 1 index.html | cut -d\" -f2))
+		ksURL=$(echo $kURL | sed 's/xz/sign/g')
+		lNAME=$(echo $kURL | cut -d\/ -f8)
+		lsNAME=$(echo $lNAME | sed 's/xz/sign/g')
+
+		wget -c $kURL
+		unxz $lNAME
+		gpg --homedir /home/asmcoder/.local/share/gnupg --verify
+
+
 
 
 
