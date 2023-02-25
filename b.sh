811981e15067fea03d0c378326b682c438b290b4
Yogendra Singh Kashyap <yskthecpplover@gmail.com>
2023-02-25 22:23:11 +0530

    Fix not moving kernel to /boot

--- b.sh
+++ b.sh
@@ -1,3 +1,4 @@
 [[ -f ".config" ]] && make -j12 && make -j12 modules && sudo make modules_install \
+        && sudo cp -v arch/x86/boot/bzImage /boot/vmlinuz-asmcoder \
 		&& sudo mkinitcpio -p linux514asmcoder \
 		&& sudo grub-mkconfig -o /boot/grub/grub.cfg
